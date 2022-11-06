49d250f9c9dfe1126dc7b9c6148092d7c73275ff
Albert Jurasik <albert.jurasik@globallogic.com>
2022-11-06 12:17:59 +0100

    modify logging for info message

--- LoggerHandler.py
+++ LoggerHandler.py
@@ -8,9 +8,9 @@ class LoggerHandler:
     def get_logger(self):
         logger = logging.getLogger(__name__)
 
-        fileHandler = logging.FileHandler('logfile.log')
+        fileHandler = logging.FileHandler('./logs/logfile.log')
         formatter = logging.Formatter('%(asctime)s :%(levelname)s : %(name)s :%(message)s')
         fileHandler.setFormatter(formatter)
         logger.addHandler(fileHandler)  # filehandler object
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(logging.INFO)
         return logger
